7646e8159e8887041a4a90d11d98bc2178878aaf
ratel4150 <rratel76@gmail.com>
2022-05-25 17:37:58 +0200

    changes

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
+/* import CustomerProfile from "./CustomerProfile"; */
+
 /* import FakeBookings from "../data/fakeBookings.json"; */
 // import SearchResults from "./SearchResults.js";
 // import FakeBookings from "./data/fakeBookings.json";
@@ -8,8 +10,28 @@ import SearchResults from "./SearchResults.js";
 const Bookings = () => {
   const search = searchVal => {
     console.info("TO DO!", searchVal);
+    if (searchVal.length > 0) {
+      setFakeBookings(
+        fakeBookings
+          .filter(result => {
+            console.log(result.firstName);
+            return (
+              result.firstName.toLowerCase() === searchVal.toLowerCase() ||
+              result.surname.toLowerCase() === searchVal.toLowerCase()
+            );
+          })
+          .map(result => {
+            return result;
+          })
+      );
+    } else if (searchVal.length === 0) {
+      console.log(setInitialBookings(initialBookings));
+      setInitialBookings(initialBookings);
+    }
   };
   const [fakeBookings, setFakeBookings] = useState(null);
+  const [initialBookings, setInitialBookings] = useState([]);
+  /* const[selectedId,setSelectedId]=useState() */
   useEffect(() => {
     console.log("Fetching data from glitch");
 
@@ -17,6 +39,9 @@ const Bookings = () => {
       .then(res => res.json())
       .then(data => {
         setFakeBookings(data);
+      })
+      .then(data1 => {
+        setInitialBookings(data1);
       });
   }, []);
 
@@ -28,7 +53,10 @@ const Bookings = () => {
           <Search search={search} />
 
           {/* <SearchResults results={FakeBookings} /> */}
-          <SearchResults results={fakeBookings} />
+          <SearchResults
+            results={fakeBookings} /* setSelectedId={setSelectedId} */
+          />
+          {/*    <CustomerProfile id={selectedId}/> */}
         </div>
       </div>
     );
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
+
 import moment from "moment";
+import CustomerProfile from "./CustomerProfile";
+
 moment().format();
+
 const SearchResults = props => {
   console.log(props);
+  const [selectedId, setSelectedId] = useState(false);
   let estates = [
     useState(false),
     useState(false),
@@ -29,127 +34,156 @@ const SearchResults = props => {
   encendido4 ? (fondo4 = "encender-fondo") : (fondo4 = "apagar-fondo");
 
   return (
-    <table className="table ">
-      <thead>
-        <tr>
-          <th scope="col">Id</th>
-          <th scope="col">Title</th>
-          <th scope="col">First Name</th>
-          <th scope="col">Surname</th>
-          <th scope="col">Email</th>
-          <th scope="col">Room Id</th>
-          <th scope="col">Check In Date</th>
-          <th scope="col">Check Out Date</th>
-          <th scope="col">Number Of Nights</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.results.map((result, index) => {
-          var b = moment(result.checkInDate);
-          var a = moment(result.checkOutDate);
+    <div>
+      <table className="table ">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Title</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Surname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room Id</th>
+            <th scope="col">Check In Date</th>
+            <th scope="col">Check Out Date</th>
+            <th scope="col">Number Of Nights</th>
+            <th scope="col">Profile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.results.map((result, index) => {
+            var b = moment(result.checkInDate);
+            var a = moment(result.checkOutDate);
 
-          switch (index) {
-            case 0:
-              return (
-                <tr
-                  key={index}
-                  className={fondo}
-                  onClick={() => setEncendido(!encendido)}
-                >
-                  <th scope="row">{result.id} </th>
-                  <td>{result.title}</td>
-                  <td>{result.firstName}</td>
-                  <td>{result.surname}</td>
-                  <td>{result.email}</td>
-                  <td>{result.roomId}</td>
-                  <td>{result.checkInDate}</td>
-                  <td>{result.checkOutDate}</td>
-                  <td>{a.diff(b, "days")}</td>
-                </tr>
-              );
+            switch (index) {
+              case 0:
+                return (
+                  <tr
+                    key={index}
+                    className={fondo}
+                    onClick={() => setEncendido(!encendido)}
+                  >
+                    <th scope="row">{result.id} </th>
+                    <td>{result.title}</td>
+                    <td>{result.firstName}</td>
+                    <td>{result.surname}</td>
+                    <td>{result.email}</td>
+                    <td>{result.roomId}</td>
+                    <td>{result.checkInDate}</td>
+                    <td>{result.checkOutDate}</td>
+                    <td>{a.diff(b, "days")}</td>
+                    <td>
+                      <button onClick={() => setSelectedId(result.id)}>
+                        Show Profile
+                      </button>
+                    </td>
+                  </tr>
+                );
 
-              break;
-            case 1:
-              return (
-                <tr
-                  key={index}
-                  className={fondo1}
-                  onClick={() => setEncendido1(!encendido1)}
-                >
-                  <td scope="row">{result.id} </td>
-                  <td>{result.title}</td>
-                  <td>{result.firstName}</td>
-                  <td>{result.surname}</td>
-                  <td>{result.email}</td>
-                  <td>{result.roomId}</td>
-                  <td>{result.checkInDate}</td>
-                  <td>{result.checkOutDate}</td>
-                  <td>{a.diff(b, "days")}</td>
-                </tr>
-              );
-              break;
+                break;
+              case 1:
+                return (
+                  <tr
+                    key={index}
+                    className={fondo1}
+                    onClick={() => setEncendido1(!encendido1)}
+                  >
+                    <td scope="row">{result.id} </td>
+                    <td>{result.title}</td>
+                    <td>{result.firstName}</td>
+                    <td>{result.surname}</td>
+                    <td>{result.email}</td>
+                    <td>{result.roomId}</td>
+                    <td>{result.checkInDate}</td>
+                    <td>{result.checkOutDate}</td>
+                    <td>{a.diff(b, "days")}</td>
+                    <td>
+                      <button onClick={() => setSelectedId(result.id)}>
+                        Show Profile
+                      </button>
+                    </td>
+                  </tr>
+                );
+                break;
 
-            case 2:
-              return (
-                <tr
-                  key={index}
-                  className={fondo2}
-                  onClick={() => setEncendido2(!encendido2)}
-                >
-                  <td scope="row">{result.id} </td>
-                  <td>{result.title}</td>
-                  <td>{result.firstName}</td>
-                  <td>{result.surname}</td>
-                  <td>{result.email}</td>
-                  <td>{result.roomId}</td>
-                  <td>{result.checkInDate}</td>
-                  <td>{result.checkOutDate}</td>
-                  <td>{a.diff(b, "days")}</td>
-                </tr>
-              );
+              case 2:
+                return (
+                  <tr
+                    key={index}
+                    className={fondo2}
+                    onClick={() => setEncendido2(!encendido2)}
+                  >
+                    <td scope="row">{result.id} </td>
+                    <td>{result.title}</td>
+                    <td>{result.firstName}</td>
+                    <td>{result.surname}</td>
+                    <td>{result.email}</td>
+                    <td>{result.roomId}</td>
+                    <td>{result.checkInDate}</td>
+                    <td>{result.checkOutDate}</td>
+                    <td>{a.diff(b, "days")}</td>
+                    <td>
+                      <button onClick={() => setSelectedId(result.id)}>
+                        Show Profile
+                      </button>
+                    </td>
+                  </tr>
+                );
 
-            case 3:
-              return (
-                <tr
-                  key={index}
-                  className={fondo3}
-                  onClick={() => setEncendido3(!encendido3)}
-                >
-                  <td scope="row">{result.id} </td>
-                  <td>{result.title}</td>
-                  <td>{result.firstName}</td>
-                  <td>{result.surname}</td>
-                  <td>{result.email}</td>
-                  <td>{result.roomId}</td>
-                  <td>{result.checkInDate}</td>
-                  <td>{result.checkOutDate}</td>
-                  <td>{a.diff(b, "days")}</td>
-                </tr>
-              );
-              break;
-            case 4:
-              return (
-                <tr
-                  key={index}
-                  className={fondo4}
-                  onClick={() => setEncendido4(!encendido4)}
-                >
-                  <td scope="row">{result.id} </td>
-                  <td>{result.title}</td>
-                  <td>{result.firstName}</td>
-                  <td>{result.surname}</td>
-                  <td>{result.email}</td>
-                  <td>{result.roomId}</td>
-                  <td>{result.checkInDate}</td>
-                  <td>{result.checkOutDate}</td>
-                  <td>{a.diff(b, "days")}</td>
-                </tr>
-              );
-              break;
-          }
-        })}
-      </tbody>
-    </table>
+              case 3:
+                return (
+                  <tr
+                    key={index}
+                    className={fondo3}
+                    onClick={() => setEncendido3(!encendido3)}
+                  >
+                    <td scope="row">{result.id} </td>
+                    <td>{result.title}</td>
+                    <td>{result.firstName}</td>
+                    <td>{result.surname}</td>
+                    <td>{result.email}</td>
+                    <td>{result.roomId}</td>
+                    <td>{result.checkInDate}</td>
+                    <td>{result.checkOutDate}</td>
+                    <td>{a.diff(b, "days")}</td>
+                    <td>
+                      <button onClick={() => setSelectedId(result.id)}>
+                        Show Profile
+                      </button>
+                    </td>
+                  </tr>
+                );
+                break;
+              case 4:
+                return (
+                  <tr
+                    key={index}
+                    className={fondo4}
+                    onClick={() => setEncendido4(!encendido4)}
+                  >
+                    <td scope="row">{result.id} </td>
+                    <td>{result.title}</td>
+                    <td>{result.firstName}</td>
+                    <td>{result.surname}</td>
+                    <td>{result.email}</td>
+                    <td>{result.roomId}</td>
+                    <td>{result.checkInDate}</td>
+                    <td>{result.checkOutDate}</td>
+                    <td>{a.diff(b, "days")}</td>
+                    <td>
+                      <button onClick={() => setSelectedId(result.id)}>
+                        Show Profile
+                      </button>
+                    </td>
+                  </tr>
+                );
+                break;
+            }
+          })}
+        </tbody>
+      </table>
+      <CustomerProfile id={selectedId} />
+    </div>
   );
 };
 
