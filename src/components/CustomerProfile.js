c9f2b5a55afd3087b638c7fcf362008ebe0cca13
ratel4150 <rratel76@gmail.com>
2022-05-25 18:23:06 +0200

    changes

--- src/components/CustomerProfile.js
+++ src/components/CustomerProfile.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 const CustomerProfile = props => {
+  const [customer, setCustomer] = useState({});
+
+  const CUSTOMER_API = "https://cyf-react.glitch.me/customers/";
+  useEffect(() => {
+    fetch(CUSTOMER_API + props.id)
+      .then(res => res.json())
+      .then(json => setCustomer(json));
+  }, [props.id]);
+
+  /* https://cyf-react.glitch.me/customers/<ID> */
   return (
     <div>
       <h3>Customer {props.id} Profile</h3>
+      <ul>
+        <li>ID:{customer.id}</li>
+        <li>Email:{customer.email}</li>
+        <li>VIP:{customer.vip ? "Yes" : "No"}</li>
+        <li>Phone Number:{customer.phoneNumber}</li>
+      </ul>
     </div>
   );
 };
